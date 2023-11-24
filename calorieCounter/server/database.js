2d46219413b66d349a7e8a6a6f846a6c5d8f8806
DanRHenry <danielrhenry@gmail.com>
2023-11-24 17:41:59 +0000

    removed extra files

--- calorieCounter/server/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const connection = "mongodb+srv://danielhenrydev:Pineapple5782*@cluster0.kagdxbf.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
