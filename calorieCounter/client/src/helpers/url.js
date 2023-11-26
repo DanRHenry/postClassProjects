9687f198143c5ba61fcf63839efe9ac795910cf0
DanRHenry <danielrhenry@gmail.com>
2023-11-26 00:24:35 +0000

    removed url

--- calorieCounter/client/src/helpers/url.js
+++
@@ -1,5 +0,0 @@
-//* URL to the backend server
-export const baseurl = 'http://192.168.68.88:4300'
-
-// export const baseurl = 'http://danhenrydev.com/api'
-//  export const baseurl = 'http://localhost:4300'
