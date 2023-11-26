8970a6b5f3bc89112315641670216869dafb91f5
DanRHenry <danielrhenry@gmail.com>
2023-11-26 15:38:49 +0000

    added meta tag to index.html, changed url.js

--- calorieCounter/client/src/helpers/url.js
+++ calorieCounter/client/src/helpers/url.js
@@ -1,5 +1,5 @@
 //* URL to the backend server
-export const baseurl = 'http://192.168.68.88:4300'
+// export const baseurl = 'https://192.168.68.88:4300'
 
-// export const baseurl = 'http://danhenrydev.com/api'
-//  export const baseurl = 'http://localhost:4300'
+export const baseurl = 'https://danhenrydev.com'
+//  export const baseurl = 'https://localhost:4300'
