9e1b2900d9fbfd6a11a47e69d0ab3bc0819e5ab9
DanRHenry <danielrhenry@gmail.com>
2023-11-26 00:27:49 +0000

    added url

---
+++ calorieCounter/client/src/helpers/url.js
@@ -0,0 +1,5 @@
+//* URL to the backend server
+export const baseurl = 'http://192.168.68.88:4300'
+
+// export const baseurl = 'http://danhenrydev.com/api'
+//  export const baseurl = 'http://localhost:4300'
