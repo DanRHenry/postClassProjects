1db8f576deb61a6d8774ce44b91f3ee203cc85d4
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-11-26 12:13:01 -0500

    trying new cors configuration

--- calorieCounter/server/app.js
+++ calorieCounter/server/app.js
@@ -51,7 +51,44 @@ app.use(cors());
 //     origin: "https://danhenrydev.com",
 //     optionsSuccessStatus: 200
 // }));
-
+// ! https://community.render.com/t/no-access-control-allow-origin-header/12947
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "*"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+  });
+// !
+//???????????????????????????????????
+// !
+app.options("*", (req, res) => {
+    console.log("preflight");
+    if (
+      req.headers.origin === "https://badmintown.onrender.com" &&
+      allowMethods.includes(req.headers["access-control-request-method"]) &&
+      allowHeaders.includes(req.headers["access-control-request-headers"])
+    ) {
+      console.log("pass");
+      return res.status(204).send();
+    } else {
+      console.log("fail");
+    }
+    })
+//!
 app.use("/user", user);
 app.use("/food", food);
 app.listen(PORT, () => log(`CalorieCounter Server on Port: ${PORT}`));
