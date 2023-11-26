3f8e0e60d6d3fb52322cf990ba58ea5cb2aed843
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-11-25 19:11:01 -0500

    changed backend url

--- calorieCounter/client/src/helpers/url.js
+++ calorieCounter/client/src/helpers/url.js
@@ -2,4 +2,4 @@
 export const baseurl = 'http://192.168.68.88:4300'
 
 // export const baseurl = 'http://danhenrydev.com/api'
-// export const baseurl = 'http://localhost:4300'
+//  export const baseurl = 'http://localhost:4300'
