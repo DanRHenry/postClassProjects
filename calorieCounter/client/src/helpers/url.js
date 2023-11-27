7898d93edb7124dd93bfda3fd9be9d01c25fdb33
DanRHenry <danielrhenry@gmail.com>
2023-11-27 00:19:51 +0000

    changed url

--- calorieCounter/client/src/helpers/url.js
+++ calorieCounter/client/src/helpers/url.js
@@ -1,5 +1,15 @@
 //* URL to the backend server
 // export const baseurl = 'https://192.168.68.88:4300'
 
-// export const baseurl = 'https://danhenrydev.com:4300'  
-export const baseurl = 'http://localhost:4300'
+
+
+ export const baseurl = 'https://danhenrydev.com:443/api'  
+
+
+
+// Redirects to machine's local (nodemon) Was working on development server, but not in production -- refers back to the host machine, not the server
+// export const baseurl = 'http://localhost:4300'
+
+
+// Didn't work:
+// export const baseurl = 'https://localhost:4300'
