73119771240999c72beb9405b9cf56a759e37903
DanRHenry <danielrhenry@gmail.com>
2023-11-29 01:22:36 +0000

    Added some placeholder values

--- calorieCounter/client/src/components/dashboard/FoodHeaders.jsx
+++ calorieCounter/client/src/components/dashboard/FoodHeaders.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useEffect, useState} from "react";
 import { Container, Row, Col } from "reactstrap";
 export default function FoodHeaders(props) {
+
+const [weightNum, setWeight] = useState(0);
+const [maintainNum, setMaintain] = useState(0);
+const [loseOne, setLoseOne] = useState(0);
+const [loseTwo, setLoseTwo] = useState(0);
+
+useEffect(() => {
+  setWeight(props.information.weight);
+  setMaintain(props.information.maintain);
+  if (props.information.maintain) {
+  setLoseOne(props.information.maintain - 500);
+  setLoseTwo(props.information.maintain - 1000);
+ }
+}, [props.information.weight])
+
+
   return (
     <>
       <Container>
@@ -9,16 +26,16 @@ export default function FoodHeaders(props) {
             Date: {`${props.month}/${props.day}/${props.year}`}
           </Col>
           <Col m="auto" className="bg-light border">
-            Weight: {`${props.information?.weight}`}
+            Weight: {weightNum}
           </Col>
           <Col m="auto" className="bg-light border">
-            Maintain: {`${props.information?.maintain}`}
+            Maintain: {maintainNum - props.dailyCalories}
           </Col>
           <Col m="auto" className="bg-light border">
-            Lose 1 lb/week: {`${props.information?.maintain - 500}`}
+            Lose 1 lb/week: {loseOne - props.dailyCalories}
           </Col>
           <Col m="auto" className="bg-light border">
-            Lose 2 lbs/week: {`${props.information?.maintain - 1000}`}
+            Lose 2 lbs/week: {loseTwo - props.dailyCalories}
           </Col>
           <Col m="auto" className="bg-light border">
             Total Calories: {props.dailyCalories}
