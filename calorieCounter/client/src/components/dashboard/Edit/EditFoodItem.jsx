45f66cec98a5fa2c204f2d2ed15f2032fdde5fcd
Dan Henry <114960108+DanRHenry@users.noreply.github.com>
2023-12-04 17:44:09 +0000

    removed extra code

--- calorieCounter/client/src/components/dashboard/Edit/EditFoodItem.jsx
+++ calorieCounter/client/src/components/dashboard/Edit/EditFoodItem.jsx
@@ -4,15 +4,7 @@ import { Form, FormGroup, Input, Button } from "reactstrap";
 import { baseurl } from "../../../helpers/url";
 
 export default function EditFoodItem(props) {
-  // const creatorNameRef = "";
   const foodNameRef = useRef();
-  // const mealCategoryRef = useRef();
-  // const unitRef = useRef();
-  // const quantityRef = useRef();
-  // const caloriesRef = useRef();
-  // const mealTypeRef = useRef();
-  // const [formInputFields, setFormInputFields] = useState();
-  // const userEmail = props.information.email;
 
   let day;
 
@@ -27,31 +19,18 @@ export default function EditFoodItem(props) {
       //* Stop the page from refreshing when the form submits
       e.preventDefault();
 
-      //* Creating a variable that holds each input's ref value
-      // const creatorName = userEmail;
-
       const date = props.year.toString() + props.month.toString() + day;
       // const creatorName = creatorNameRef.current.value;
 
       const foodName = foodNameRef.current.value;
       const mealCategory = "mealCategoryRef.current.value"; //IE Breakfast, lunch, dinner, snack
-      // const unit = unitRef.current.value;
-      // const quantity = quantityRef.current.value;
-      // const calories = caloriesRef.current.value;
       const mealType = "mealTypeRef.current.value"; // ie: Protein, sugary snack, etc.
 
-      // setFormInputFields(
-
-      // )
       //* The server expects json, we need to build and json-ify a user object to send to our server
       let newFoodObj = JSON.stringify({
-        // creatorName,
         date,
         foodName,
         mealCategory,
-        // unit,
-        // quantity,
-        // calories,
         mealType,
       });
 
@@ -72,12 +51,10 @@ export default function EditFoodItem(props) {
         // Build an async fetch, fetch will use the url and requestOptions obj
         const response = await fetch(url, requestOptions);
         const data = await response.json();
-        //   console.log("data:", data);
 
         // If the server send a success message we can proceed
 
         if (data.message === "Success! Food Saved!") {
-          // props.setFoodData();
           props.getFoodInformation();
           newFoodObj = {};
           const form = document.querySelector("form")
@@ -100,27 +77,6 @@ export default function EditFoodItem(props) {
             type="text"
             autoComplete="off"
           />
-          {/* <Input
-            name="Amount"
-            placeholder="Amount"
-            innerRef={quantityRef}
-            type="number"
-            autoComplete="off"
-          />
-          <Input
-            name="Unit"
-            placeholder="Unit"
-            innerRef={unitRef}
-            type="text"
-            autoComplete="off"
-          />
-          <Input
-            name="Calories"
-            placeholder="Calories"
-            innerRef={caloriesRef}
-            type="number"
-            autoComplete="off"
-          /> */}
         </FormGroup>
       </Form>
       <Button id="submit" onClick={handleSubmit}>
