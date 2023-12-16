5eaffe4503e25c0e64d86a7676b3ecbce94ab5a9
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-12-16 04:53:33 -0500

    changed connection

--- calorieCounter/server/database.js
+++ calorieCounter/server/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const connection = "mongodb+srv://danielhenrydev:Pineapple5782*@cluster0.kagdxbf.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+const CONNECTION = process.env.CONNECTION;
+const MONGO = process.env.MONGODB;
+
+mongoose.connect(CONNECTION,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
