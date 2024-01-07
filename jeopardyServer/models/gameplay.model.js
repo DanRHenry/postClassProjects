3b0ef5f07f80ca074797473c381018aa3bfd9d15
Dan Henry <114960108+DanRHenry@users.noreply.github.com>
2024-01-07 19:37:22 +0000

    Began work on a gameplay controller and model, for custom games

---
+++ jeopardyServer/models/gameplay.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const GameplaySchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }
+})
